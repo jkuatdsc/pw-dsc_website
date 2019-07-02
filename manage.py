2939e2545349947338c4fe4aa687dc5e8afbbb11
jonnieey <johnjahi55@gmail.com>
2019-07-02 18:53:57 +0300

    Create test runner helper
    - Add stand-in helper to run tests
    * Should implement to run tests via flask-cli

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
+import subprocess
+
 from flask_script import Manager, Server
 from core import create_app
 
 manager = Manager(create_app)
-
 # run flask server at (0.0.0.0:5000)
 manager.add_command('runserver', Server())
+
+## improve to use flask-cli command
+@manager.command
+def test():
+    "Run tests"
+    subprocess.run(['nose2', '-v'])
 
 if __name__ == '__main__':
     manager.run()
