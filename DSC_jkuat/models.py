5f2033c27f194ff4f2224d5a35238e59e91a7ed7
jonnieey <johnjahi55@gmail.com>
2019-07-02 19:38:44 +0300

    Create Article model (basic)

---
+++ DSC_jkuat/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+from core import db
+from accounts.models import User
+
+class Article(db.Document):
+    """
+    Model for article collection
+    """
+    title = db.StringField(
+        required = True,
+        max_length = 30
+    )
+    description = db.StringField(
+        required = True,
+        max_length = 80
+    )
+    author = db.ReferenceField(
+        'User',
+        required = True,
+        # Nullify the relationship between article
+        # and author when user is deleted
+        reverse_delete_rule = db.NULLIFY
+    )
+    created = db.ComlexDateTimeField(
+        default = datetime.utcnow()
+    )
+    # Implement to support images, and other media 
+    content = db.StringField(
+        required = True
+    )
+    ## others to implement
+    # updated
+    # ratings
+
+    meta = {
+        'collection': 'articles',
+        'ordering': ['-created']
+    }
+
+
+    
+
