426d158c6b52d4fa23f3fc5ecfaa46b50db330c9
jonnieey <johnjahi55@gmail.com>
2019-07-04 16:38:22 +0300

    Refactor blog tests

--- tests/unit_tests/test_blog.py
+++ tests/unit_tests/test_blog.py
@@ -42,47 +42,45 @@ class ArticleTestCase(TestCase):
             json = user
         )
         return res
+    def create_article(self, login_res=None, article=None):
+        headers = '' if not login_res else {
+            'Authorization': 'Bearer %s' % (login_res.json['refresh_token'])
+            }
+        new_article = self.test_client.post(
+            self.url_helper('article'),
+            headers = headers,
+            json = article
+        )
+        return new_article
 
     def test_user_can_create_article(self):
         self.register(USER1)
-        refresh_token = self.login(USER1).json['refresh_token']
+        login_res = self.login(USER1)
 
-        res = self.test_client.post(
-            self.url_helper('article'),
-            headers = {'Authorization': f"Bearer {refresh_token}"},
-            json = ARTICLE
-        )
+        res = self.create_article(login_res, ARTICLE)
         self.assertEqual(res.status_code, 201)
 
     def test_user_can_create_article_without_content(self):
         self.register(USER1)
-        refresh_token = self.login(USER1).json['refresh_token']
+        login_res = self.login(USER1)
 
         cust_art = ARTICLE.copy()
         cust_art.pop('content')
 
-        res = self.test_client.post(
-            self.url_helper('article'),
-            headers = {'Authorization': f"Bearer {refresh_token}"},
-            json = cust_art
-        )
+        res = self.create_article(login_res, cust_art)
         self.assertEqual(res.status_code, 400)
     
     def  test_create_article_without_login(self):
-        res = self.test_client.post(
-            self.url_helper('article')
-        )
+        res = self.create_article(article=ARTICLE) 
         self.assertEqual(res.status_code, 401)
     
     def test_get_article_by_id(self):
         self.register(USER1)
-        refresh_token = self.login(USER1).json['refresh_token']
-        new_article = self.test_client.post(
-            self.url_helper('article'),
-            headers = {'Authorization': 'Bearer %s' % (refresh_token)},
-            json = ARTICLE
-        )
+        login_res = self.login(USER1)
+
+        new_article = self.create_article(login_res, ARTICLE)
         new_article_pk = new_article.json['article']['_id']['$oid']
+
         res = self.test_client.get(
             self.url_helper('articles/%s' % (new_article_pk))
         )
@@ -90,14 +88,11 @@ class ArticleTestCase(TestCase):
     
     def test_get_all_articles(self):
         self.register(USER1)
-        refresh_token = self.login(USER1).json['refresh_token']
+        login_res = self.login(USER1)
 
         for article in range(0, 5):
-           self.test_client.post(
-            self.url_helper('article'),
-            headers = {'Authorization': 'Bearer %s' % (refresh_token)},
-            json = ARTICLE
-           )
+            self.create_article(login_res, ARTICLE)
+
         res = self.test_client.get(
             self.url_helper('articles')
         )
@@ -108,21 +103,18 @@ class ArticleTestCase(TestCase):
         user1 = self.register(USER1)
         user2 = self.register(USER2)
 
-        refresh_token1 = self.login(USER1).json['refresh_token']
-        refresh_token2 = self.login(USER2).json['refresh_token']
+        login_res_1 = self.login(USER1)
+        login_res_2 = self.login(USER2)
 
         for user in USER1, USER2:
             if user ==  USER1:
-                refresh_token = refresh_token1
+                login_res = login_res_1
             else:
-                refresh_token = refresh_token2
+                login_res = login_res_2
 
             for article in range(3):
-                self.test_client.post(
-                    self.url_helper('article'),
-                    headers={'Authorization': 'Bearer %s' % (refresh_token)},
-                    json = ARTICLE
-                )
+                self.create_article(login_res, ARTICLE)
+
         res = self.test_client.get(
             self.url_helper('/users/anderson/articles')
         )
